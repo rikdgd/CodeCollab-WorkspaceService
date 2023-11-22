8f06c95b239feb67269dce64990d9f15d9e26c3d
Rik de Goede <rik.dgd@gmail.com>
2023-11-22 17:49:30 +0100

    Extract messageType and commandName from message bus message

--- Utils/BasicMessageHandler.cs
+++ Utils/BasicMessageHandler.cs
@@ -1,7 +1,9 @@
+using System.Dynamic;
 using System.Text.Json;
 using CodeCollab___WorkspaceService.Interfaces;
 using CodeCollab___WorkspaceService.Models;
 using CodeCollab___WorkspaceService.Services;
+using MongoDB.Bson.Serialization.Serializers;
 
 namespace CodeCollab___WorkspaceService.Utils;
 
@@ -11,21 +13,30 @@ public class BasicMessageHandler : IMessageHandler
     {
         try
         {
-            dynamic? jsonMessage = JsonSerializer.Deserialize<dynamic>(message);
-            if (jsonMessage == null) return; 
+            Dictionary<string, object>? jsonMessage = JsonSerializer.Deserialize<Dictionary<string, object>>(message);
+            // if (jsonMessage == null || jsonMessage == "") return; 
             
-            var messageType = jsonMessage.MessageType;
-            var commandName = jsonMessage.CommandName;
-            var payload = jsonMessage.Payload;
-            
-            WorkspaceModel workspace = new WorkspaceModel()
-            {
-                Name = payload.Name,
-                OwnerId = payload.OwnerId
-            };
+            string messageType = jsonMessage["MessageType"].ToString();
+            string commandName = jsonMessage["CommandName"].ToString();
+            var payload = jsonMessage["Payload"];// as IDictionary<string, object>;
 
+            if (payload is Dictionary<string, object> payloadDict)
+            {
+                var Name = payloadDict["Name"];
+                var OwnerId = payloadDict["OwnerId"];
+            }
+            else
+            {
+                string name = "failed...";
+            }
+            // WorkspaceModel workspace = new WorkspaceModel()
+            // {
+            //     Name = payload["Name"],
+            //     OwnerId = payload["OwnerId"]
+            // };
+            
             WorkspaceService service = new WorkspaceService();
-            service.CreateWorkspace(workspace);
+            // service.CreateWorkspace(workspace);
         }
         catch (Exception e)
         {
